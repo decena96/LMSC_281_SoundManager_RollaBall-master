43b712646e02d6cad29a43bf986060a3f6228783
decena96 <Decena@22-stu-wrls-172-30-36-196.berklee.net>
2015-11-23 14:03:32 -0500

    Michael Decena V1
    Added code to lower volume on the game.

---
+++ Assets/Scripts/NearEnd.cs
@@ -0,0 +1,30 @@
+﻿/*using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NearEnd : MonoBehaviour {
+
+
+	//To Determine players
+	public Text P1;
+	public Text P2;
+
+	//The audio for sound clip to play when score is at 5 or passed 5.
+	AudioSource intense;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ((P1.text == 5) || (P2.text == 5))
+		{
+			intense.mute = false;
+		}
+	}
+}*/
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿//LMSC-281 Logic and Programming
+//Fall 2015 Jeanine Cowen
+
+using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	 
+	 //Garett Schmidt - Variabes for pitch and Amplutide automation
+    public float amp = 1f;
+    public float pch = 1f;
+	
+	//since we know we will only have one sound manager in the game we can create a static instance
+	public static SoundManager instance;
+
+	//if we want to limit the ability for outside classes to make changes we can use the following code
+	//this sets our class to basic read-only status
+	//	public static SoundManager instance { 
+	//		get; 
+	//		private set; 
+	//	}
+	
+	// This will make the class available when the level loads
+	void Awake () {
+		instance = this;
+	}
+	
+	//this function/method will check to make sure the audiosource on the passed in object is not already playing
+	//if it is playing it will not play, otherwise it will
+	public void PlayObjectSoundOnce (GameObject thisObject) {
+		
+		if (!thisObject.GetComponent<AudioSource>().isPlaying) {
+			thisObject.GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	//to use this function in another script use this code:
+	//SoundManager.instance.PlayObjectSoundOnce (this.gameObject);
+	//Garett Schmidt - Function to randomize pitch and volume
+
+    void PlayRandomPitchAndVolume()
+    {
+        //Pitch and Amplitude Randomization.
+        amp = Random.Range(0.9f, 1.1f);
+        pch = Random.Range(0.8f, 1.2f);
+        GetComponent<AudioSource>().volume = amp;
+        GetComponent<AudioSource>().pitch = pch;
+        GetComponent<AudioSource>().Play();
+    }
+}
